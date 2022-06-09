fe95cd29332bb01872bd8a5c9c7fe3b1ced3595f
Cory Baker <corybaker24@gmail.com>
2022-06-08 19:34:57 -0700

    breadth fist and depth first traversal through graph

--- graph-practice.js
+++ graph-practice.js
@@ -32,3 +32,54 @@ const adjList = {
   console.log("Third Test:")
   printDepthFirst(4); // Prints 1 through 6 in Depth-first order, starting with 4
                       // One possible solution:  4, 6, 5, 2, 1, 3
+
+
+
+
+ const adjList = {
+  1: [2, 5],
+  2: [1, 3, 5],
+  3: [2, 4],
+  4: [3, 5, 6],
+  5: [1, 2, 4],
+  6: [4]
+}
+
+function printBreadthFirst(start) {
+    debugger
+    // create a queue with the start pliugged in
+    // create a set for nodes already visited with start inside, use {}
+    let queue = [start] // 3
+    let visited = [start] // 3
+
+    //WHILE the queue.length
+    //dequeue the first node in queue and set it to current node
+    while (queue.length) { //true
+        let currentNode = queue.shift(); //3
+        console.log(currentNode); //3,
+
+        for (let neighbors of adjList[currentNode]) {
+
+            if (!visited.includes(neighbors)) {
+                queue.push(neighbors);
+                // console.log(queue, 'queue')
+                visited.push(neighbors)
+                // console.log(visited,' visited')
+            }
+
+
+        }
+
+    }
+}
+
+
+console.log("First Test:")
+printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
+                      // One possible solution:  3, 2, 4, 1, 5, 6
+console.log("Second Test:")
+printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
+                      // One possible solution:  6, 4, 3, 5, 2, 1
+console.log("Third Test:")
+printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
+                      // One possible solution:  4, 3, 5, 6, 2, 1
